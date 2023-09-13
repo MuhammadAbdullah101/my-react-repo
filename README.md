0fb64cf92b523a50a451a7f4e9dda52b33c6ccaf
Muhammad Abdullah <abdullah.ehsan@systemsltd.com>
2023-09-13 16:16:52 +0500

    Namaste React Episode 2

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # My React Repo
+
+# parcel 
+
+- Dev Build
+- Local server
+- HMR = Hot Module Replacment
+- HMR is using File Watching Algorithim - 
+- File Watching Algorithim : written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- content Hashing
+- Code splitting
+- Differntial Bundling -Support Older Browser
+- Diagnostics
+- Error Handling
+-  Https
+- Tree shaking - remove unused code
--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
 const divWith2h1 = React.createElement(
   "div",
   { id: "MyDiv" },
-  React.createElement("h1", { id: "heading" }, "Hello"),
-  React.createElement("h2", { id: "heading2" }, "World")
-);
+  [React.createElement("h1", { id: "heading" }, "Hello"),
+  React.createElement("h2", { id: "heading2" }, "Abdullah This is React")
+  ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(divWith2h1);
